159b6d63aedea4d719a10e5c3e1d3b9d5ac6529f
abuzak0 <kairzhanarsen@gmail.com>
2023-07-07 22:00:18 +0600

    modif JS form

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const { form } = document.forms;
+
+const isRadio = (type) => ["checkbox", "radio"].includes(type);
+
+function getFormValue(event) {
+  event.preventDefault();
+
+  const { elements } = form;
+  const values = {};
+
+  for (let i = 0; i < elements.length; i++) {
+    const fromElement = elements[i];
+    const { name } = fromElement;
+
+    if (name) {
+      const { value, type, checked } = fromElement;
+
+      values[name] = isRadio(type) ? checked : value;
+    }
+  }
+
+  console.log(values);
+}
+
+form.addEventListener("submit", getFormValue);
+
+/*
+let user = [];
+  for (let elem of form.elements) user.push(elem.value);
+  console.log(user);*/
+
+/*const form = document.getElementById("form");
+
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
   form.addEventListener("submit", formSend);
 
   async function formSend(e) {
@@ -7,9 +39,9 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+
+
+
+
 const formBtn = document.querySelector(".form-button");
-formBtn.addEventListener("click", () => {
-  let user = [];
-  for (let elem of form.elements) user.push(elem.value);
-  console.log(user);
-});
+formBtn.addEventListener("click", () => {}); */
